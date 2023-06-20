f706c69d289af455e5b1bb9f03ca9c39bfc33f33
Seb <sebastianneill2011@gmail.com>
2023-06-20 19:58:47 +0100

    added secure-online for chikes staff

--- js/home.js
+++ js/home.js
@@ -1,7 +1,12 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    h = document.getElementById("popdown").style.height;
+    document.getElementById("popdown").style.visibility = "hidden";
+    document.getElementById("popdown").style.height = 0;
+}
+function so(){
+    document.getElementById("popdown").style.visibility = "visible";
+    document.getElementById("popdown").style.height = h;
 }
